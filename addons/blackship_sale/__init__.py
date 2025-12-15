686e2a841be476fc1e516ea0a98b6d73bbb549e9
Pietro di Caprio <pietrodc0@outlook.com>
2025-12-15 10:19:36 +0100

    Add scaffold for blackship_sale addon

---
+++ addons/blackship_sale/__init__.py
@@ -0,0 +1,2 @@
+# Initialize BlackShip Sales addon
+from . import models
---
+++ addons/blackship_sale/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "BlackShip Sales",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "BlackShip Sales customizations",
+    "depends": ["crm", "sale", "contacts", "account"],
+    "data": [],
+    "application": True,
+    "license": "LGPL-3",
+}
